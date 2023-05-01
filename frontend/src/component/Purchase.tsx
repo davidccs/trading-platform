1d924a890b6e8f0d9cddd63f3ee7b39c1ce80b44
david <ddchen30@gmail.com>
2023-05-01 10:54:15 +1000

    improved UI

--- frontend/src/component/Purchase.tsx
+++ frontend/src/component/Purchase.tsx
@@ -1,4 +1,6 @@
 import {
+	Alert,
+	AlertIcon,
 	Box,
 	Button,
 	Flex,
@@ -8,17 +10,20 @@ import {
 	HStack,
 	Input,
 	Radio,
-	RadioGroup, SimpleGrid, Stack, Text, useColorModeValue
+	RadioGroup,
+	SimpleGrid,
+	Stack,
+	Text,
+	useColorModeValue,
 } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import UserPorfolio from "./UserPorfolio";
-
 
 interface Item {
 	customerId: number;
 	name: string;
 	balance: number;
-    portfolioCount: number;
+	portfolioCount: number;
 }
 
 export default function SignupCard() {
@@ -26,7 +31,7 @@ export default function SignupCard() {
 	const [price, setPrice] = useState("");
 	const [quantity, setQuantity] = useState("");
 	const [orderType, setValue] = useState("BUY");
-
+	const [showError, setError] = useState<boolean>();
 
 	const handleSymbolChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 		setSymbol(event.target.value);
@@ -47,25 +52,28 @@ export default function SignupCard() {
 			symbol: symbol,
 			price: price,
 			quantity: quantity,
-		}
+		};
 
 		var transactionType = orderType.toLowerCase();
 
-		const url = `http://localhost:8080/api/stocks/${transactionType}?symbol=${symbol}&price=${price}&quantity=${quantity}`
+		const url = `http://localhost:8080/api/stocks/${transactionType}?symbol=${symbol}&price=${price}&quantity=${quantity}`;
 
 		e.preventDefault();
 		try {
-			const response = await fetch(
-				url,
-				{
-					method: "POST",
-					headers: { "Content-Type": "application/json" },
-					body: JSON.stringify(formData),
-				}
-			);
-			console.log("Response:", response);
+			const response = await fetch(url, {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify(formData),
+			});
+			if (!response.ok) {
+				throw new Error("Response not valid");
+			}
+			setError(false);
+			setTimeout(() => {
+				window.location.reload(); 
+			}, 1500);
 		} catch (error) {
-			console.error(error);
+			setError(true);
 		}
 	};
 
@@ -80,7 +88,7 @@ export default function SignupCard() {
 		};
 		fetchItems();
 	}, []);
-    const customerInformation = Object.values(items);
+	const customerInformation = Object.values(items);
 
 	return (
 		<Flex
@@ -88,90 +96,106 @@ export default function SignupCard() {
 			justify={"center"}
 			bg={useColorModeValue("gray.50", "gray.800")}>
 			<form onSubmit={handleSubmit}>
-			<SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
-				<Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
-				<Stack align={"center"}>
-						<Heading fontSize={"4xl"} textAlign={"center"}>
-							Currently Holding
-						</Heading>
-					</Stack>
-				<UserPorfolio></UserPorfolio>
-				</Stack>
-				<Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
-					<Stack align={"center"}>
-						<Heading fontSize={"4xl"} textAlign={"center"}>
-							Buying or Selling?
-						</Heading>
-						
-						<Text fontSize='6xs'>Current Funds: ${customerInformation[2]?.toString()} (AUD) </Text>
-					</Stack>
-					<Box
-						rounded={"lg"}
-						bg={useColorModeValue("white", "gray.700")}
-						boxShadow={"lg"}
-						p={8}>
-						<Stack spacing={4}>
-							<RadioGroup onChange={setValue} value={orderType}>
-								<HStack>
-									<Stack spacing={4} direction="row">
-										<Radio value="BUY">Buy</Radio>
-										<Radio value="SELL">Sell</Radio>
-									</Stack>
-								</HStack>
-							</RadioGroup>
-							<HStack>
-								<Box>
-									<FormControl id="Symbol">
-										<FormLabel>Symbol</FormLabel>
-										<Input
-											required
-											type="text"
-											value={symbol}
-											onChange={handleSymbolChange}
-										/>
-									</FormControl>
-								</Box>
-								<Box>
-									<FormControl id="Quantity">
-										<FormLabel>Quantity</FormLabel>
-										<Input
-											required
-											type="number"
-											value={quantity}
-											onChange={handleQuantityChange}
-										/>
-									</FormControl>
-								</Box>
-							</HStack>
-							<FormControl id="price">
-								<FormLabel>$ Price</FormLabel>
-								<Input
-									required
-									type="number"
-									value={price}
-									onChange={handlePriceChange}
-								/>
-							</FormControl>
-
-							<Stack spacing={10} pt={2}>
-								<Button
-									type="submit"
-									loadingText="Submitting"
-									size="lg"
-									bg={"green.400"}
-									color={"white"}
-									_hover={{
-										bg: "green.500",
-									}}
-									onClick={() => window.location.reload()}>
-									{orderType}
-								</Button>
-							</Stack>
+				<SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
+					<Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+						<Stack align={"center"}>
+							<Heading fontSize={"4xl"} textAlign={"center"}>
+								Currently Holding
+							</Heading>
 						</Stack>
-					</Box>
-				</Stack>
-				</SimpleGrid>
+						<UserPorfolio></UserPorfolio>
+					</Stack>
+					<Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
+						<Stack align={"center"}>
+							<Heading fontSize={"4xl"} textAlign={"center"}>
+								Buying or Selling?
+							</Heading>
 
+							<Text fontSize="6xs">
+								Current Funds: $
+								{customerInformation[2]?.toString()} (AUD){" "}
+							</Text>
+						</Stack>
+						<Box
+							rounded={"lg"}
+							bg={useColorModeValue("white", "gray.700")}
+							boxShadow={"lg"}
+							p={8}>
+							<Stack spacing={4}>
+								<RadioGroup
+									onChange={setValue}
+									value={orderType}>
+									<HStack>
+										<Stack spacing={4} direction="row">
+											<Radio value="BUY">Buy</Radio>
+											<Radio value="SELL">Sell</Radio>
+										</Stack>
+									</HStack>
+								</RadioGroup>
+								<HStack>
+									<Box>
+										<FormControl id="Symbol">
+											<FormLabel>Symbol</FormLabel>
+											<Input
+												required
+												type="text"
+												value={symbol}
+												onChange={handleSymbolChange}
+											/>
+										</FormControl>
+									</Box>
+									<Box>
+										<FormControl id="Quantity">
+											<FormLabel>Quantity</FormLabel>
+											<Input
+												required
+												type="number"
+												value={quantity}
+												onChange={handleQuantityChange}
+											/>
+										</FormControl>
+									</Box>
+								</HStack>
+								<FormControl id="price">
+									<FormLabel>$ Price</FormLabel>
+									<Input
+										required
+										type="number"
+										value={price}
+										onChange={handlePriceChange}
+									/>
+								</FormControl>
+
+								<Stack spacing={10} pt={2}>
+									<Button
+										type="submit"
+										loadingText="Submitting"
+										size="lg"
+										bg={"green.400"}
+										color={"white"}
+										_hover={{
+											bg: "green.500",
+										}}>
+										{orderType}
+									</Button>
+								</Stack>
+								{showError && showError !== undefined && (
+									<Alert status="error">
+										<AlertIcon />
+										There was an error processing your
+										request
+									</Alert>
+								)}
+								{!showError && showError !== undefined && (
+									<Alert status="success">
+										<AlertIcon />
+										Transaction Successful
+									</Alert>
+								)}
+							</Stack>
+						</Box>
+					</Stack>
+				</SimpleGrid>
 			</form>
 		</Flex>
 	);
